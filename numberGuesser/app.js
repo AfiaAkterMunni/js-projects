02c6dda65ef72ee67f00c125f970a921ff317469
AfiaAkterMunni <afiamunni03@gmail.com>
2025-03-16 16:07:22 +0600

    modify

--- numberGuesser/app.js
+++ numberGuesser/app.js
@@ -1,64 +1,114 @@
-const form = document.querySelector('#form');
-const number = document.querySelector('#number');
+//guess value
+let min = 1,
+    max = 10,
+    winningNum = 2;
+guessLeft = 3;
 
-const randomNumber = Math.floor(Math.random() * 10) + 1;
-console.log('Random number ' + randomNumber);
+//ui elements
+const game = document.querySelector('#game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('.max-num'),
+    guessInput = document.querySelector('#guess-input'),
+    guessBtn = document.querySelector('#guess-btn'),
+    message = document.querySelector('.message');
 
-for (let i = 0; i < 3; i++) {
-    form.addEventListener('submit', getResult);
-    function getResult(e) {
-        const x = 3 - (i + 1);
-        if (x > 0) {
-            if (number.value) {
-                if (randomNumber === number.value) {
-                    console.log(`${number.value} is correct, YOU WIN!`);
-                }
-                else {
-                    console.log(`${number.value} is not correct, ${x} guesses left`);
-                }
-            }
-            else {
-                console.log(`NaN is not correct, ${x} guesses left`);
-            }
-        }
+//asign ui min and max 
+minNum.textContent = min;
+maxNum.textContent = max;
 
-        else {
-            console.log(`Game Over, you lost. The correct number was ${randomNumber}`);
-        }
-        e.preventDefault();
+//listen for guess
+guessBtn.addEventListener('click', function () {
+    let guess = parseInt(guessInput.value);
+
+    // validate
+    if (isNaN(guess) || guess < min || guess > max) {
+        setmessage(`Please enter a number between ${min} and ${max}`);
     }
 
+
+});
+
+// set message
+function setmessage(msg) {
+    message.style.color = 'red';
+    message.textContent = msg;
 }
 
 
 
 
-// let guess = 3;
-// while (guess) {
-//     guess -= 1;
-//     console.log(guess);
 
-//     form.addEventListener('submit', getResult);
-//     function getResult(e) {
 
+
+
+// const form = document.querySelector('#form');
+// const number = document.querySelector('#number');
+
+// const randomNumber = Math.floor(Math.random() * 10) + 1;
+// console.log('Random number ' + randomNumber);
+
+// const guess = 3;
+
+// form.addEventListener('submit', getResult);
+// const text = "";
+// function getResult(e) {
+//     const x = 3 - (i + 1);
+//     if (x > 0) {
 //         if (number.value) {
-
 //             if (randomNumber === number.value) {
-//                 console.log(`${number.value} is correct, YOU WIN!`);
+//                 text += `${number.value} is correct, YOU WIN!`;
+//                 console.log(text);
 //             }
 //             else {
-//                 console.log(`${number.value} is not correct, ${x} guesses left`);
+//                 text += `${number.value} is not correct, ${x} guesses left`;
+//                 console.log(text);
 //             }
 //         }
 //         else {
-//             console.log(`NaN is not correct, ${x} guesses left`);
+//             text += `NaN is not correct, ${x} guesses left`;
+//             console.log(text);
 //         }
-//         e.preventDefault();
 //     }
+
+//     else {
+//         text += `Game Over, you lost. The correct number was ${randomNumber}`;
+//         console.log(text);
+//     }
+//     e.preventDefault();
 // }
 
+// // document.getElementById("demo").innerHTML = text;
 
 
 
-// const number = Math.floor(Math.random() * 10) + 1;
-// console.log(number);
+
+
+// // let guess = 3;
+// // while (guess) {
+// //     guess -= 1;
+// //     console.log(guess);
+
+// //     form.addEventListener('submit', getResult);
+// //     function getResult(e) {
+
+// //         if (number.value) {
+
+// //             if (randomNumber === number.value) {
+// //                 console.log(`${number.value} is correct, YOU WIN!`);
+// //             }
+// //             else {
+// //                 console.log(`${number.value} is not correct, ${x} guesses left`);
+// //             }
+// //         }
+// //         else {
+// //             console.log(`NaN is not correct, ${x} guesses left`);
+// //         }
+// //         e.preventDefault();
+// //     }
+// // }
+
+
+
+
+// // const number = Math.floor(Math.random() * 10) + 1;
+// // console.log(number);
